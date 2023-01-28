bb6bc201682193911a596d95d918811ff3995889
anoop027 <74642411+anoop027@users.noreply.github.com>
2023-01-29 00:15:49 +0530

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/ubuntu/k8s
-aws eks --region us-east-2 update-kubeconfig --name "demo2-eks.us-east-2.eksctl.io"
+aws eks --region us-east-2 update-kubeconfig --name demo2-eks
 kubectl apply -f client-cluster-ip-service.yml
